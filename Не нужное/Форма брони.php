3aa5b8b8cd2283cd5eaebab4236b2e04a371c97b
Misha-Kamyshev <kam628326@gmail.com>
2021-10-09 20:03:58 +1000

    Изменения
    1. Добавлены фото кондиционеров Midea.
    2. Перенесена форма брони и css к ней в папку "Не нужное"
    3. Изменено расширение с .php на .html

---
+++ Не нужное/Форма брони.php
@@ -0,0 +1,43 @@
+<div class="form">
+
+	<form action="../php/form.php" method="post" class="bron">
+
+		<div>
+			<label>Имя</label>
+			<input type="text" name="name" placeholder="Введите имя">
+		</div>
+
+		<div>	
+			<label>Фамилия</label>
+			<input type="text" name="secondname" placeholder="Введите фамилию">
+		</div>
+
+		<div>	
+			<label>Номер телефона</label>
+			<input type="text" name="phone" placeholder="+7 999 999 99 99">
+		</div>
+
+		<div>	
+			<label>E-mail</label>
+			<input type="text" name="email" placeholder="E-mail@gmail.com">
+		</div>
+
+		<div>	
+			<label>Адрес</label>
+			<input type="text" name="address" placeholder="Введите адрес">
+		</div>
+
+		<div>	
+			<label>Время</label>
+			<input type="time" name="time" placeholder="Введите дату">
+		</div>
+
+		<div>	
+			<label>Дата</label>
+			<input type="date" name="data" placeholder="Введите дату">
+		</div>
+
+
+		<input type="submit" value="Отправить" class="button">
+	</form>
+</div>
