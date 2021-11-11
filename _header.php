eb69fd980649e532a9dcb7d20f123aabb4369363
Misha-Kamyshev <kam628326@gmail.com>
2021-11-11 19:39:59 +1000

    Изменение кнопки "цены" на "каталог"

--- _header.php
+++ _header.php
@@ -22,7 +22,7 @@
             <ul>
                 <div>
                     <li class="shop"> <a href="bron.php">БРОНИРОВАНИЕ</a> </li>
-                    <li class="shop"> <a href="shop.php">ЦЕНЫ</a> </li>
+                    <li class="shop"> <a href="shop.php">КАТАЛОГ</a> </li>
                     <li> <a href="about.php">О НАС</a> </li>
                     <li> <a href="index.php">ГЛАВНАЯ</a> </li>
                 </div>
@@ -36,7 +36,7 @@
             <div id="myDropdown" class="dropdown-links">
                 <a href="index.php">Главная</a>
                 <a href="about.php">О нас</a>
-                <a href="shop.php">Цены</a>
+                <a href="shop.php">Каталог</a>
                 <a href="bron.php">Бронирование</a>
             </div>
         </div>
